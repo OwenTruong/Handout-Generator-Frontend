eec1d6e1bb54c7b3eb6eec3f5f567f7682d109ba
OwenTruong <44269025+OwenTruong@users.noreply.github.com>
2023-03-06 22:01:52 -0500

    change readme and get project set up with eslint, prettier, tailwindcss, and jest

---
+++ website/.eslintrc.js
@@ -0,0 +1,45 @@
+module.exports = {
+  env: {
+    browser: true,
+    'jest/globals': true,
+    es2021: true,
+  },
+  extends: ['plugin:react/recommended', 'standard-with-typescript', 'prettier'],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.json',
+  },
+  plugins: ['react', 'prettier', 'jest'],
+  env: {
+    browser: true,
+    es6: true,
+    jest: true,
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2021,
+    sourceType: 'module',
+    project: './tsconfig.json',
+  },
+  rules: {
+    'linebreak-style': 'off',
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto',
+      },
+    ],
+  },
+
+  // overrides: [
+  //   {
+  //     files: ['**/*.ts?(x)'],
+  //     rules: {},
+  //   },
+  // ],
+};
---
+++ website/src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App(): JSX.Element {
+  return (
+    <div className="App text-3xl font-bold text-blue-500 text-center">
+      Hello World
+    </div>
+  );
+}
+
+export default App;
