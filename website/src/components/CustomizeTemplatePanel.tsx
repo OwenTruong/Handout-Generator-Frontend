e515869b60f1f83cb6a22ff756e3807239aeac00
OwenTruong <44269025+OwenTruong@users.noreply.github.com>
2023-03-07 21:42:57 -0500

    get components down

---
+++ website/src/components/CustomizeTemplatePanel.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+type Props = {};
+
+export function CustomizeTemplatePanel(props: Props): JSX.Element {
+  return <div>CustomizeTemplatePanel</div>;
+}
---
+++ website/src/components/IconSubPanel.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+interface Props {}
+
+export function IconSubPanel(props: Props): JSX.Element {
+  return <div>IconSubPanel</div>;
+}
