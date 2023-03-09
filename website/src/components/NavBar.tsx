d0d994f86932f40cdde82c9766ba4504dcf877d7
OwenTruong <44269025+OwenTruong@users.noreply.github.com>
2023-03-08 21:33:36 -0500

    add empty navbar component

---
+++ website/src/components/NavBar.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+interface Props {}
+
+export function NavBar(props: Props): JSX.Element {
+  return <div>NavBar</div>;
+}
