303098d3993fcc78d1ca56e234d5ba5f11dbeb3e
OwenTruong <44269025+OwenTruong@users.noreply.github.com>
2023-03-09 21:20:20 -0500

    add imports

--- website/src/App.tsx
+++ website/src/App.tsx
@@ -1,9 +1,19 @@
 import React from 'react';
+// import { Route, Routes, Navigate } from 'react-router-dom';
+
+import { ConfirmationPanel } from 'components/ConfirmationPanel';
+import { CreatePDFPanel } from 'components/CreatePDFPanel';
+import { CustomizeTemplatePanel } from 'components/CustomizeTemplatePanel';
+import { Guide } from 'components/Guide';
+import { Login } from 'components/Login';
+import { NavBar } from 'components/NavBar';
 
 function App(): JSX.Element {
   return (
-    <div className="App text-3xl font-bold text-blue-500 text-center">
-      Hello World
+    <div className="App">
+      <h1>Hello World</h1>
+      <NavBar />
+      <Login />
     </div>
   );
 }
--- website/src/components/Login.tsx
+++ website/src/components/Login.tsx
@@ -3,6 +3,8 @@ import React from 'react';
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
 interface Props {}
 
+// Allow people to skip login/singup and go to next panel. Panel comes in from the right to create a new stack. Panel fizzles out to the left to remove it from stack (only fizzle for login/signup and help panel for now). Fizzling should only be used for panels that are temporary and are not part of the main chain of panels.
+
 export function Login(props: Props): JSX.Element {
   return <div>Login</div>;
 }
