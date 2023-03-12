ff032de3a2f8a81fee940f490ef1d8d5655cad95
OwenTruong <44269025+OwenTruong@users.noreply.github.com>
2023-03-11 21:35:29 -0500

    add login comp

--- website/src/App.tsx
+++ website/src/App.tsx
@@ -1,18 +1,15 @@
 import React from 'react';
-// import { Route, Routes, Navigate } from 'react-router-dom';
 
-import { ConfirmationPanel } from 'components/ConfirmationPanel';
-import { CreatePDFPanel } from 'components/CreatePDFPanel';
-import { CustomizeTemplatePanel } from 'components/CustomizeTemplatePanel';
-import { Guide } from 'components/Guide';
+// import { ConfirmationPanel } from 'components/ConfirmationPanel';
+// import { CreatePDFPanel } from 'components/CreatePDFPanel';
+// import { CustomizeTemplatePanel } from 'components/CustomizeTemplatePanel';
+// import { Guide } from 'components/Guide';
 import { Login } from 'components/Login';
-import { NavBar } from 'components/NavBar';
 
 function App(): JSX.Element {
   return (
     <div className="App">
       <h1>Hello World</h1>
-      <NavBar />
       <Login />
     </div>
   );
--- website/src/components/Login.tsx
+++ website/src/components/Login.tsx
@@ -5,6 +5,30 @@ interface Props {}
 
 // Allow people to skip login/singup and go to next panel. Panel comes in from the right to create a new stack. Panel fizzles out to the left to remove it from stack (only fizzle for login/signup and help panel for now). Fizzling should only be used for panels that are temporary and are not part of the main chain of panels.
 
+const LoginComponent = (): JSX.Element => {
+  return (
+    <form>
+      <input type="text" placeholder="Email" />
+      <input type="hidden" placeholder="Password" />
+    </form>
+  );
+};
+
+const SignUpComponent = (): JSX.Element => {
+  return (
+    <form>
+      <input type="text" placeholder="Email" />
+      <input type="password" placeholder="Password" />
+      <input type="password" placeholder="Confirm Password" />
+    </form>
+  );
+};
+
 export function Login(props: Props): JSX.Element {
-  return <div>Login</div>;
+  return (
+    <div>
+      <h3>Login Panel</h3>
+      <SignUpComponent />
+    </div>
+  );
 }
